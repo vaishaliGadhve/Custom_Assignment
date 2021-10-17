30619b40a6d05b4fb92d500d2fe122ca21cd36d3
vaishaliGadhve <vaishugadave@gmail.com>
2021-10-17 19:31:16 +0530

    Add files

---
+++ tailwind.config.js
@@ -0,0 +1,137 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    
+  colors: {
+                  tranparent: 'transparenr',
+                  current: 'currentColor',
+                indigo:{
+                  light: '#b3bcf5',
+                  DEFAULT:'#5c6ac4',
+                  dark:'#202e78',
+                },
+                red: {
+                  light: '#F87171',
+                  DEFAULT:'#FCA5A5',
+                  dark:'#7F1D1D',
+                },
+                green: {
+                  light: '#6EE7B7',
+                  DEFAULT:'#34D399',
+                  dark:'#064E3B',
+                },
+                grey: {
+                  light: '#e5e7eb',
+                  DEFAULT:'#9ca3af',
+                  dark:'#111827',
+                },
+                purple: {
+                  light: '#C7D2FE',
+                  DEFAULT:'#818CF8',
+                  dark:'#4C1D95',
+                },
+                yellow: {
+                  light: '#FdE68A',
+                  DEFAULT:'#FBBF24',
+                  dark:'#78350F',
+                },
+                pink: {
+                  light: '#FBCFE8',
+                  DEFAULT:'#EC4899',
+                  dark:'#BE185D',
+                },
+                
+                
+    },
+     fontFamily:{
+            'sans':['ui-sans-serif','system-ui'],
+            'serif':['ui-serif','georgia'],
+            'mono':['ui-monospace','SFMono-Regular'],
+
+     },
+  fontSize:{
+              'xs':'.75rem',
+              'sm':'.875rem',
+              'tiny':'875rem',
+              'base':'1rem',
+              'lg':'1.125rem',
+              'xl':'1.25rem',
+              '2xl':'1.5rem',
+              '3xl':'1.875rem',
+              '4xl':'2.25rem',
+              '5xl':'3rem',
+              '6xl':'4rem',
+              '7xl':'5rem',
+       },
+    spacing :{
+              sm:'8px',
+              md:'16px',
+              lg: '24px',
+              xl: '48px',
+      },
+     
+  marging :{
+          sm:'8px',
+          md:'16px',
+          lg: '24px',
+          xl: '48px',
+       },
+  width:{
+         '1/7':'14.2857143%',
+         '2/7': '28.5714286%',
+         '3/7': '42.8571429',
+         '4/7': '57.1428571',
+         '5/7': '85.7142857',
+
+       },
+  minwidth :{
+         '0':'0',
+         '1/4':'25%',
+         '1/2':'50%',
+         '3/4':'75%',
+         'full':'100%'
+        },
+  maxwidth :{
+          '1/4':'25%',
+          '1/2':'50%',
+          '3/4':'75%',
+         },
+  height :{
+          sm:'8px',
+          md:'16px',
+          lg: '24px',
+          xl: '48px',
+         },
+  minHeight :{
+          '0':'0',
+          '1/4':'25%',
+          '1/2':'50%',
+          '3/4':'75%',
+          'full':'100%'
+         },
+ maxHeight :{
+          '0':'0',
+          '1/4':'25%',
+          '1/2':'50%',
+          '3/4':'75%',
+          'full':'100%'
+         },
+  padding :{
+          sm:'8px',
+          md:'16px',
+          lg: '24px',
+          xl: '48px',
+         },
+     
+      
+},
+    extend: {
+     
+    },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
